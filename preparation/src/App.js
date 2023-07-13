d79f44fca8866891171ab357dd944152cc07826b
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-07-13 17:52:43 +0530

    button

--- preparation/src/App.js
+++ preparation/src/App.js
@@ -1,18 +1,35 @@
+import "./App.css";
+import { Component } from "react";
 
-import './App.css';
-import {Component} from "react";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
 
+  componentDidMount() {
+    console.log("componentDidMount : Inital render");
+  }
 
-class App extends Component{
+    increment(){
 
-     render(){
+        this.setState({count:this.state.count + 1})
+    }
 
-        return(
-             <>
-                 <h1>HEllo</h1> 
-            </>
-        )
-     }
+  render() {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+
+        <button onClick={()=>this.increment()}>Click</button>
+
+        {/* <button onClick={this.increment.bind(this)}>Click</button> */}
+
+      </>
+    );
+  }
 }
 
 export default App;
