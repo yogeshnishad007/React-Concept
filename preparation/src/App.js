2c59b4ed6560a5aef255a1e9fcf0f89a407b53b1
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-07-13 17:41:58 +0530

    class component

---
+++ preparation/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import {Component} from "react";
+
+
+class App extends Component{
+
+     render(){
+
+        return(
+             <>
+                 <h1>HEllo</h1> 
+            </>
+        )
+     }
+}
+
+export default App;
